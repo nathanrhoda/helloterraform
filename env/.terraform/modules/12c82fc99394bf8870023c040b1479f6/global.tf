5675596b8422b6a8f854da0229a7347ed7b51b22
Nathan Rhoda (ZA) <nathan.rhoda@absacapital.com>
2018-06-21 14:54:18 +0200

    removed state files

--- env/.terraform/modules/12c82fc99394bf8870023c040b1479f6/global.tf
+++
@@ -1,9 +0,0 @@
-variable "server_port" {}
-variable "sg_launchconfig" {}
-variable "asg" {}
-variable "elb" {}
-variable "sg_elb" {}
-
-variable "user_data" {}
-
-data "aws_availability_zones" "all" {}
